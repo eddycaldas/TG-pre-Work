5d8c224cf06999a58f2c8908876e8e86e8798046
Eddy Caldas <eddycaldas@live.com>
2018-08-28 12:46:20 -0600

    Javascript - strig interpolation

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
-var morningAlarm = '6:30AM';
-morningAlarm = '6:59AM';
-console.log('Morning alarm is set to: ', morningAlarm);
+var favoriteAnimal = 'dog';
+console.log('My favorite animal: '+ favoriteAnimal)
