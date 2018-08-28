5edfa11406df3e9330d705a38208c00664596344
Eddy Caldas <eddycaldas@live.com>
2018-08-28 12:23:40 -0600

    console

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-var myString = undefined;
-var myNumber = undefined;
-var myBoolean = undefined;
-
-
-// Do not edit the code under this line
-console.log("Name: " + myString);
-console.log("Lucky Number: " + myNumber);
-console.log("Good joke? " + myBoolean);
+console.log('ham, pineapple, jalapenos')
