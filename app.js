bb46c23ea6fb531f81568d451c47eecc68779ea5
Eddy Caldas <eddycaldas@live.com>
2018-08-28 12:41:51 -0600

    Javascript - create variables II

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var strength = '50,000 punds';
+var strength = '50,000 pounds';
+var age = 26;
+var hasPet = true;
+
+console.log(hasPet);
+console.log(age);
 console.log('How much stuff can a variable hold? ', strength);
