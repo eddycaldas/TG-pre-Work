aaa537d92d1a7725cbc6f775f7a4c3dbab9f52b1
Eddy Caldas <eddycaldas@live.com>
2018-08-28 13:06:41 -0600

    Javascript - Introduction to control flow

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-console.log('JavaScript');
-console.log(33.7);
-console.log(true);
-console.log(null);
+let userName = 'Picachu';
+let knowsJavaScript = true;
+
+if (knowsJavaScript && userName) {
+  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+} else if (knowsJavaScript) {
+  console.log('Great! Get ready to practice your JavaScript!');
+} else if (userName) {
+  console.log('Great, ' + userName + '! Get ready to learn something new!');
+} else {
+  console.log('Great! Get ready to learn something new!');
+}
