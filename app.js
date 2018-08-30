bc9055f29fdcdbce2dbdd90fbceffb644e6e6c73
Eddy Caldas <eddycaldas@live.com>
2018-08-29 20:53:55 -0600

    Javascript - console

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-var myString = 'string';
-var myNumber = 578;
-var myBoolean = false;
-
-
-// Do not edit the code under this line
-console.log("Name: " + myString);
-console.log("Lucky Number: " + myNumber);
-console.log("Good joke? " + myBoolean);
+console.log('ham', 'bacon')
