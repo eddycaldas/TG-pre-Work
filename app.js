322a2b92c5ce0336efde235470856986c2e75bae
Eddy Caldas <eddycaldas@live.com>
2018-08-29 22:23:37 -0600

    Javascript - for loops

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ var vacationSpots = ['Ocean', 'Mountains', 'Deserts'];
 console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
+
+for(var i = 0; i < vacationSpots.length; i++) {
+	console.log('I would love to visit '  + vacationSpots[i]);
+}
