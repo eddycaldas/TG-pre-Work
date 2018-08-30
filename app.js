725fb183ad1c01d278b79250d614fc91cd126644
Eddy Caldas <eddycaldas@live.com>
2018-08-29 22:38:03 -0600

    Javascript - for loops inside of for loops

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var vacationSpots = ['Ocean', 'Mountains', 'Deserts'];
-console.log(vacationSpots[0]);
-console.log(vacationSpots[1]);
-console.log(vacationSpots[2]);
+var myPlaces = ['Italy', 'Norway', 'Sweden'];
+var friendPlaces = ['France', 'Norway', 'Russia'];
 
-for(var i = vacationSpots.length - 1; i >= 0; i--) {
-	console.log('I would love to visit '  + vacationSpots[i]);
+for (var i=0; i<myPlaces.length; i++) {
+	//console.log(myPlaces[i]);
+  for(var j=0; j<friendPlaces.length; j++) {
+		//console.log(friendPlaces[j]);
+    if(myPlaces[i] === friendPlaces[j]) {
+      console.log(myPlaces[i], friendPlaces[j]);
+    }
+  }
 }
