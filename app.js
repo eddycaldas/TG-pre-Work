c72569b63d702f82740fb7daa3f391c038b0d085
Eddy Caldas <eddycaldas@live.com>
2018-08-29 21:18:32 -0600

    Javascript - Functional scope

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var laundryRoom = 'Basement';
 var mailRoom = 'Room 1A';
-console.log('Laundry: ' + laundryRoom + ', Mail: ' + mailRoom);
+
+var myApartment = function() {
+  var mailBoxNumber = 'Box 3';
+  var laundryRoom = 'In-unit';
+  console.log('Mail box: ' + mailBoxNumber + ', Laundry:' + laundryRoom);
+};
+
+myApartment();
