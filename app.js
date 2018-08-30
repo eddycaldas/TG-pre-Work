d4d4ed77ca7f293a394a5e95046f06737ecb971b
Eddy Caldas <eddycaldas@live.com>
2018-08-29 22:27:10 -0600

    Javascript - for loops backwards

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
 
-for(var i = 0; i < vacationSpots.length; i++) {
+for(var i = vacationSpots.length - 1; i >= 0; i--) {
 	console.log('I would love to visit '  + vacationSpots[i]);
 }
