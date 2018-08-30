22d5404ee7334ae110dc1154e5ab04a1962cc733
Eddy Caldas <eddycaldas@live.com>
2018-08-29 20:46:06 -0600

    return

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+var orderCount = 0;
+
 function takeOrder(topping, crustType) {
+  orderCount = orderCount + 1;
 	console.log('Order: ' + crustType + ' pizza topped with ' + topping)
-}
+};
+
+function getSubTotal(itemCount) {
+  return itemCount*7.5;
+};
+
+
 takeOrder('ham', 'original');
 takeOrder('bacon', 'thin crust');
 takeOrder('jalapenos', 'thin crust');
+
+console.log(getSubTotal(orderCount));
+
