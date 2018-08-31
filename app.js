0a2a129bd3660ac3ae0e591d8aae96bf33732662
Eddy Caldas <eddycaldas@live.com>
2018-08-30 21:56:18 -0600

    JS Introduction - Console

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-function main() {
-  $('.skillset').hide();
-  $('.skillset').fadeIn(1000);
-  
-  $('.projects').hide();
-  
-  $('.projects-button').on('click', function() {
-		$(this).next().slideToggle(400);
-    $(this).toggleClass('active');
-    $(this).text('Projects Viewed');
-	});
-}
-
-$(document).ready(main);
+console.log('23');
+console.log('99')
