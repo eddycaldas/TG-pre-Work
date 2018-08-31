ecf7efe3b9b8fa6f753c41b270de059a475bb2af
Eddy Caldas <eddycaldas@live.com>
2018-08-30 21:28:45 -0600

    jQuery - next

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ function main() {
 	$(".skillset").hide();
   $(".skillset").fadeIn(1000);
   $(".projects").hide();
+  
   $(".projects-button").on('click', function() {
-    $('.projects').toggle();
+    $(this).next().toggle();
     $(this).toggleClass('active')
   })
 };
