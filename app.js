dbd41011379701685e4d58ba33662440eba737b6
Eddy Caldas <eddycaldas@live.com>
2018-08-31 09:16:31 -0600

    JS Introduction - String Interpolation; templete literals

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-var favoriteAnimal = 'dog';
-console.log('My favorite animal: ' + favoriteAnimal);
+var myName = 'Eddy';
+var myCity = 'Denver';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
