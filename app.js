ab05bb6ea4ec0c1c2a12b26747874207ed5ec8fd
Eddy Caldas <eddycaldas@live.com>
2018-09-01 12:11:50 -0600

    JS Introduction - Looping in reverse

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-// Write your code below
-for (let counter = 5; counter < 11; counter++) {
-	console.log(counter);
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter)
 };
-
-
