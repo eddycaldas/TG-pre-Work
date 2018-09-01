b4600a697610583576a56c471b3d4706f1ecde5e
Eddy Caldas <eddycaldas@live.com>
2018-09-01 17:04:44 -0600

    JS Introduction - Accessing properties

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-// Write your fasterShip object literal below
-var fasterShip = {
+let spaceship = {
+  homePlanet: 'Earth',
   color: 'silver',
-  'Fuel Type': 'Turbo Fuel'
-}
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+var crewCount = spaceship.numCrew;
+var planetArray = spaceship.flightPath;
