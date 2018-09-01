027386ae262bad438c17d6d8a895787987bce8a7
Eddy Caldas <eddycaldas@live.com>
2018-09-01 11:01:46 -0600

    JS Introduction - Parameters and arguments

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-function sayThanks() {
-	console.log('Thank you for your purchase! We appreciate your business.');
+function sayThanks(name) {
+  console.log('Thank you for your purchase ' +  name + '! We appreciate your business.');
 };
-sayThanks();
-sayThanks();
-sayThanks();
+sayThanks('Cole');
