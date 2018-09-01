915d9292cbe088586d4022a7a8228a8a754733a3
Eddy Caldas <eddycaldas@live.com>
2018-09-01 17:32:41 -0600

    JS Introduction - Nested objects

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
-
-// Write your code below
-
-let alienShip = {
-  retreat() {
-    console.log(retreatMessage)
+let spaceship = {
+  passengers: [{name: 'Space Dog'}], 
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
   },
-  takeOff() {
-    console.log('Spim... Borp... Glix... Blastoff!')
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    backup: {
+      battery: "Lithium",
+      terabytes: 50
+    }
   }
-};
+}; 
 
-alienShip.retreat();
+let capFave = spaceship.crew.captain['favorite foods'][0];
 
-alienShip.takeOff();
+let firstPassenger = spaceship.passengers[0];
