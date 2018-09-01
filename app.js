7faa30e70684f680ac4760b1c47445c9f94272c5
Eddy Caldas <eddycaldas@live.com>
2018-09-01 11:19:25 -0600

    JS Introducting - Helper function ( a function within a function

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function monitorCount(rows, columns) {
-	return rows * columns;
+  return rows * columns;
 };
-const numOfMonitors = monitorCount(5,4);
-console.log(numOfMonitors);
+
+function costOfMonitors (rows, columns) {
+	return monitorCount(rows, columns) * 200;
+};
+
+const totalCost = costOfMonitors(5,4);
+console.log(totalCost);
