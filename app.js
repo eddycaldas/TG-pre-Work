12632eb6cbd94866283f5550208cfd5506ad19c5
Eddy Caldas <eddycaldas@live.com>
2018-09-01 13:10:20 -0600

    HS Introduction - Create an array

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-// Write your code below
-let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
-let tinasFollowers = ['Sam', 'Marta', 'Elle'];
-let mutualFollowers = [];
-
-for (let i = 0; i < bobsFollowers.length; i++) {
-  for (let j = 0; j < tinasFollowers.length; j++) {
-    if (bobsFollowers[i] === tinasFollowers[j]) {
-      mutualFollowers.push(bobsFollowers[i]);
-      console.log(bobsFollowers[i])
-    }
-  }
-};
-
+const hobbies = ['sleep', 'eat', 'esat again'];
+console.log(hobbies);
