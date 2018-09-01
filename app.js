2fa3cfd833b70d358efdadbdecd1c01dd4268d47
Eddy Caldas <eddycaldas@live.com>
2018-09-01 13:15:04 -0600

    JS Introduction - Update elements

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
-const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
-var listItem = famousSayings[0];
-console.log(famousSayings[2]);
-console.log(famousSayings[3]);
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados';
