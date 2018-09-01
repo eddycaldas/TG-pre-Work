96b5cf9380f4f2309178bb26162ea01e9a726766
Eddy Caldas <eddycaldas@live.com>
2018-09-01 17:02:18 -0600

    JS Introduction - Creating object literals

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
-
-// Write you code below
-for (let i = 0; i < rapperArray.length; i++){
-  console.log(rapperArray[i])
-  if (rapperArray[i] ===  'Notorious B.I.G.'){
-    break;
-  }
+// Write your fasterShip object literal below
+var fasterShip = {
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel'
 }
-console.log("And if you don't know, now you know.")
-
-
-
-
