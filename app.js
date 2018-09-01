457947c3d282991f5c92ec7a0359421879d03528
Eddy Caldas <eddycaldas@live.com>
2018-09-01 12:57:05 -0600

    JS Introduction - Nested loop

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const vacationSpots = ['Bali', 'Paris', 'Tulum'];
-
 // Write your code below
-for( let i=0 ; i<vacationSpots.length; i++ ) {
-  console.log('I would love to visit ' + vacationSpots[i])
-}
+let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
+let tinasFollowers = ['Sam', 'Marta', 'Elle'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+      console.log(bobsFollowers[i])
+    }
+  }
+};
+
