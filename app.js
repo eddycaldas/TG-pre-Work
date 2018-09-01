775d2cfb5f91d12544b3da0ae3248a013eab7b42
Eddy Caldas <eddycaldas@live.com>
2018-09-01 17:24:17 -0600

    JS Introduction - Methods

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-let spaceship = {
-  'Fuel Type' : 'Turbo Fuel',
-  homePlanet : 'Earth',
-  color: 'silver',
-  'Secret Mission' : 'Discover life outside of Earth.'
-};
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
 
 // Write your code below
 
-spaceship.color = 'glorious gold';
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
 
-spaceship.numEngines = 9;
+alienShip.retreat();
 
-delete spaceship['Secret Mission'];
+alienShip.takeOff();
