11eadb8ea84689f8b7fb63d6ee77a8abc082f535
Eddy Caldas <eddycaldas@live.com>
2018-09-01 11:25:45 -0600

    JS Introduction - Arrow function

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const plantNeedsWater = function(day) {
-	if(day === 'Wednesday') {
-		return true;
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
   } else {
     return false;
   }
 };
-
-console.log(plantNeedsWater('Tuesday'));
