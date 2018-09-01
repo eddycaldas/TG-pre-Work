eadc10e2db7ef3a79fdd739c53ea8c9b993200f1
Eddy Caldas <eddycaldas@live.com>
2018-09-01 13:18:36 -0600

    JS Introduction - Array with let and const

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-let groceryList = ['bread', 'tomatoes', 'milk'];
-groceryList[1] = 'avocados';
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+condiments = ['mayo'];
+console.log(condiments);
+utensils[3] = 'Spoon';
+console.log(utensils);
