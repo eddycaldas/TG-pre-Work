37c56127960087f2650fa78e53886bfee1cf24c7
Eddy Caldas <eddycaldas@live.com>
2018-09-01 13:35:32 -0600

    JS Introduction - Function as data

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var numberClusters = [[1,2], [3,4], [5,6]];
-const target = numberClusters[2][1];
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+// Write your code below
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+is2p2();
+console.log(is2p2.name);
