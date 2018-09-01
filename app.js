e9f12d72cda9ba0282b1c5b866a4e9e410db07ab
Eddy Caldas <eddycaldas@live.com>
2018-09-01 16:51:24 -0600

    JS Introduction - do... while statements

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const cards = ['diamond', 'spade', 'heart', 'club'];
-
 // Write your code below
-let currentCard;
+let cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
 
-while ( currentCard != 'spade') {
-  currentCard = cards[Math.floor(Math.random() * 4)];
-	console.log(currentCard);
-}
+do {
+ cupsAdded++
+} while (cupsAdded < cupsOfSugarNeeded);
 
 
