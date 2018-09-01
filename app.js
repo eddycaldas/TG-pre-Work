860376b4f590c7fea1ee2b6b87a547d7575e6ef9
Eddy Caldas <eddycaldas@live.com>
2018-09-01 14:41:47 -0600

    JS Introduction - The map() method

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-// Iterate over fruits below
-fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+// Create the secretMessage array below
+
+const secretMessage = animals.map(animal => {
+  return animal[0];
+});
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(bigNumber => {
+  return bigNumber/100;
+});
