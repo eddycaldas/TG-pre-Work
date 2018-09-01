ddb4af89f63904bf1521b196ccfa5ac153677ce7
Eddy Caldas <eddycaldas@live.com>
2018-09-01 10:56:33 -0600

    JS Introduction - Function declarations

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-let athleteFinalPosition = 'first place';
-switch (athleteFinalPosition){
-  case 'first place':
-    console.log('You get the gold medal!');
-    break;
-  case 'second place':
-    console.log('You get the silver medal!');
-    break;
-  case 'third place':
-		console.log('You get the bronze medal!');
-    break;
-	  default:
-    console.log('No medal awarded.');
-    break;
+function getReminder() {
+	console.log('Water the plants.');
+};
+
+function greetInSpanish() {
+	console.log('Buenas Tardes.');
 }
