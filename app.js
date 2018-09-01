36b0884ba4d7278090e4830db19d90166857d1a3
Eddy Caldas <eddycaldas@live.com>
2018-09-01 11:14:43 -0600

    JS Introduction - return

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
-  console.log(`Remember to buy ${item1}`);
-  console.log(`Remember to buy ${item2}`);
-  console.log(`Remember to buy ${item3}`);
-}
+function monitorCount(rows, columns) {
+	return rows * columns;
+};
+const numOfMonitors = monitorCount(5,4);
+console.log(numOfMonitors);
