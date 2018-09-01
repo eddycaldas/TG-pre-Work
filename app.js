8a0032a4d852dd63c31301a139860ccdc5409ac5
Eddy Caldas <eddycaldas@live.com>
2018-09-01 10:20:37 -0600

    JS Introduction - Truthy and falsy assignments

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-let newVariable = 'Playing around with typeof.';
-console.log(typeof newVariable);
-newVariable = 1;
+let tool = '';
+tool = 'marker';
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
