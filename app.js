0592da16ef237f1bb09016e28ad875e92a48d7f2
Eddy Caldas <eddycaldas@live.com>
2018-09-01 17:59:05 -0600

    JS Introduction - The this keyword

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
-let spaceship = {
-    crew: {
-    captain: { 
-        name: 'Lily', 
-        degree: 'Computer Engineering', 
-        cheerTeam() { console.log('You got this!') } 
-        },
-    'chief officer': { 
-        name: 'Dan', 
-        degree: 'Aerospace Engineering', 
-        agree() { console.log('I agree, captain!') } 
-        },
-    medic: { 
-        name: 'Clementine', 
-        degree: 'Physics', 
-        announce() { console.log(`Jets on!`) } },
-    translator: {
-        name: 'Shauna', 
-        degree: 'Conservation Science', 
-        powerFuel() { console.log('The tank is full!') } 
-        }
-    }
-}; 
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo() { 
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+  }
+}
 
-// Write your code below
-
-for (let crewMember in spaceship.crew) {
-  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
-};
-
-for (let crewMember in spaceship.crew) {
-  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
-};
+console.log(robot.provideInfo());
