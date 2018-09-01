fc356ac0bda35bd48e524ce4a89ffd69572f65d4
Eddy Caldas <eddycaldas@live.com>
2018-09-01 10:25:36 -0600

    JS Introduction - Ternary operator

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-let tool = '';
-tool = 'marker';
-// Use short circuit evaluation to assign  writingUtensil variable below:
-let writingUtensil = tool || 'pen';
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
 
 
-console.log(`The ${writingUtensil} is mightier than the sword.`);
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+(favoritePhrase === 'Love That!') ?
+  console.log('I love that!') : console.log("I don't love that!");
