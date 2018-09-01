8c9161d54b73de0d876d6c571bd9649724cb5b71
Eddy Caldas <eddycaldas@live.com>
2018-09-01 15:14:31 -0600

    JS Introduction - Iterator documentation

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const newNumbers = [1, 3, 5, 7];
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
 
-const newSum = newNumbers.reduce((accumulator, currentValue) => {
-  console.log('The value of accumulator: ', accumulator);
-  console.log('The value of currentValue: ', currentValue);
-  return accumulator + currentValue;
-}, 10);
+// Something is missing in the method call below
 
-console.log(newSum);
+console.log(words.some(word => {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+const interestingWords = words.filter((word) => {return word.length > 5});
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every((word) => {return word.length > 5}));
