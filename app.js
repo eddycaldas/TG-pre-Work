43beb3436c84691bcd3e18ebb21a6e910dd81a64
Eddy Caldas <eddycaldas@live.com>
2018-09-01 10:31:25 -0600

    JS Introduction - Else if Statements

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let isLocked = false;
+let season = 'summer';
 
-isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
-
-
-let isCorrect = true;
-
-isCorrect ? console.log('Correct!') : console.log('Incorrect!');
-
-
-let favoritePhrase = 'Love That!';
-
-(favoritePhrase === 'Love That!') ?
-  console.log('I love that!') : console.log("I don't love that!");
+if (season === 'spring'){
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+	console.log('It\'s winter! Everything is covered in snow.');
+} 
+	else if ( season === 'fall') {
+	console.log('It\'s fall! Leaves are falling!');
+  } else if (season === 'summer') {
+	console.log('It\'s sunny and warm because it\'s summer!');
+  } else {
+  console.log('Invalid season.');
+}
