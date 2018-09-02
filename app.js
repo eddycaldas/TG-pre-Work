d8a91e4ba5b0197988a7f2ad0672241691867363
Eddy Caldas <eddycaldas@live.com>
2018-09-01 22:51:33 -0600

    JS Introduction - named imports

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
 
-let availableAirplanes = [
-  {
-    name: 'AeroJet',
-    fuelCapacity: 800,
-    availableStaff: ['pilots',     		'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators']
-  },
-  	{
-    	name: 'SkyJet',
-  	  fuelCapacity: 500,
-      availableStaff: ['pilots','flightAttendants']
-    }
-];
-let flightRequirements = {
-	requiredStaff: 4,
-};
-function meetsStaffRequirements(availableStaff, requiredStaff) {
-	if(availableStaff.length >= requiredStaff) {
-		return true;
-  } else {
-    return false;
-  }
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
 }
 
-export { availableAirplanes, flightRequirements, meetsStaffRequirements };
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
