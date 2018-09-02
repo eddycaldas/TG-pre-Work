83ce5de9cfd213d9b2655024cf2fd8beb3552d92
Eddy Caldas <eddycaldas@live.com>
2018-09-01 18:47:51 -0600

    JS Introduction - Destructured Assignment

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-function robotFactory(model, mobile){
-  return {
-    model,
-    mobile,
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
     beep() {
       console.log('Beep Boop');
-    }
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
   }
-}
+};
 
+const { functionality } = robot;
 
-// To check that the property value shorthand technique worked:
-const newRobot = robotFactory('P-501', false)
-console.log(newRobot.model)
-console.log(newRobot.mobile)
+functionality.beep();
