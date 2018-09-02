b0a559cf837299ab4cb2b048a238b092c9cb3ecb
Eddy Caldas <eddycaldas@live.com>
2018-09-01 18:07:21 -0600

    JS Introduction - privacy

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const robot = {
-  energyLevel: 100,
-  checkEnergy() {
-    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this.energyLevel}%.`)
   }
-}
+};
+robot._energyLevel = 'high';
+console.log(robot.recharge());
 
-robot.checkEnergy();
+
