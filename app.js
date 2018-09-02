9e71fbf30ccd40a8a9275a766bc8ca79c3f04d93
Eddy Caldas <eddycaldas@live.com>
2018-09-01 23:13:21 -0600

    JS Introduction - export as

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-import { meetsSpeedRangeRequirements, availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+export { availableAirplanes as aircrafts, flightRequirements as flightReqs, meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs };
 
-function displayFuelCapacity() {
-  availableAirplanes.forEach(function(element) {
-    console.log('Fuel Capacity of ' + element['name'] + ': ' + element['fuelCapacity']);
-  });
+let availableAirplanes = [
+{name: 'AeroJet',
+ fuelCapacity: 800,
+ availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+ maxSpeed: 1200,
+ minSpeed: 300
+}, 
+{name: 'SkyJet',
+ fuelCapacity: 500,
+ availableStaff: ['pilots', 'flightAttendants'],
+ maxSpeed: 800,
+ minSpeed: 200
 }
+];
 
-displayFuelCapacity();
+let flightRequirements = {
+  requiredStaff: 4,
+  requiredSpeedRange: 700
+};
 
-function displayStaffStatus() {
-  availableAirplanes.forEach(function(element) {
-   console.log(element['name'] + ' meets staff requirements: ' + meetsStaffRequirements(element['availableStaff'], flightRequirements['requiredStaff']) );
-  });
-}
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-displayStaffStatus();
-
-function displaySpeedRangeStatus() {
-	availableAirplanes.forEach(function(element) {
-		console.log(element.name + ' meets speed range requirements: ' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
-    });
-}
-displaySpeedRangeStatus();
-
-
-
-
-
+function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+  let range = maxSpeed - minSpeed;
+  if (range > requiredSpeedRange) {
+    return true;
+    } else {
+    return false;
+  }
+};
